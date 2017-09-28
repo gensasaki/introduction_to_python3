b6e9bf94359bb4f63f8b9e823bae9474e0ff6dca
Gen Sasaki <sasakigen11@gmail.com>
2017-09-27 21:37:59 -0700

    first commit

---
+++ youtube2.py
@@ -0,0 +1,6 @@
+import requests
+url = "http://raw.githubusercontent.com/koki0702/introducing-python/master/dummy_api/youTube_top_rated.json"
+response = requests.get(url)
+data = response.json()
+for video in data['feed']['entry'][0:6]:
+    print(video['title']['$t'])
